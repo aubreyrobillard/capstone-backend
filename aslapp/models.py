fbcb5e07f9a65316eed2d7a0797d46c930d4d808
Aubrey Robillard <aubreyrobillard@Aubreys-MacBook-Pro.local>
2023-10-16 22:40:30 -0500

    Add blank field option for notes in model

--- aslapp/models.py
+++ aslapp/models.py
@@ -3,6 +3,6 @@ from django.db import models
 class Sign(models.Model):
     image = models.CharField(max_length=800)
     name = models.CharField(max_length=100)
-    notes = models.CharField(max_length=2000)
+    notes = models.CharField(max_length=2000, blank=True)
     date = models.DateField(auto_now_add=True)
 
